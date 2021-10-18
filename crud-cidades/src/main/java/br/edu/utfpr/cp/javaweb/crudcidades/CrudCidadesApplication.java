18546f21cb5047794b1caee4dc38561999d11046
fernando <setec.solda@gmail.com>
2021-10-18 14:52:02 -0300

    alteracao

--- crud-cidades/src/main/java/br/edu/utfpr/cp/javaweb/crudcidades/CrudCidadesApplication.java
+++ crud-cidades/src/main/java/br/edu/utfpr/cp/javaweb/crudcidades/CrudCidadesApplication.java
@@ -1,4 +1,4 @@
-package br.edu.ftpr.espjava.crudcidades;
+package br.edu.utfpr.cp.javaweb.crudcidades;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- crud-cidades/src/test/java/br/edu/utfpr/cp/javaweb/crudcidades/CrudCidadesApplicationTests.java
+++ crud-cidades/src/test/java/br/edu/utfpr/cp/javaweb/crudcidades/CrudCidadesApplicationTests.java
@@ -1,4 +1,4 @@
-package br.edu.ftpr.espjava.crudcidades;
+package br.edu.utfpr.cp.javaweb.crudcidades;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

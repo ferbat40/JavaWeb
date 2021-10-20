e0df5126da49d4a7ac14a84f4b3443ec9c55bc82
fernando <setec.solda@gmail.com>
2021-10-20 11:02:08 -0300

    alter

--- crud-cidades/src/main/java/br/edu/utfpr/cp/javaweb/crudcidades/visao/CidadesController.java
+++ crud-cidades/src/main/java/br/edu/utfpr/cp/javaweb/crudcidades/visao/CidadesController.java
@@ -1,31 +1,55 @@
 package br.edu.utfpr.cp.javaweb.crudcidades.visao;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CidadesController {
+	
+	private Set<Cidades> cidade;
+	
+	public CidadesController() {
+		cidade = new HashSet();
+		
+	}
 
  
 	@GetMapping("/")
 	public String listar(Model memoria) {
-		
-		var cidade = Set.of(
-				
-				new Cidades("Piraciaba","SP"),
-				new Cidades("Itu","SP"),
-				new Cidades("Praia Grande","SP")
-				
-				);
+	
 		
 		memoria.addAttribute("ListarCidades",cidade);
 		
 		return "/crud";
+	}
+	
+	
+	@PostMapping("/criar")
+	public String criar(Cidades cidades) {
+		cidade.add(cidades);
+		return "redirect:/";
+	}
+	
+	
+	@GetMapping("/excluir")
+	public String excluir(
+			@RequestParam String nome,
+			@RequestParam String estado
+			) {
 		
 		
+		cidade.removeIf(cidadeAtual -> 
+				cidadeAtual.getNome().equals(nome) &&
+				cidadeAtual.getEstado().equals(estado)
+				);
+		
+		return "redirect:/";
 	}
 	
 }

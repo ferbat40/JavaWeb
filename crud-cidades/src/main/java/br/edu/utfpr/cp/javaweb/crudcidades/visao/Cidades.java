a217571c4a73d2efd09798bb67bb4ed21476627c
fernando <setec.solda@gmail.com>
2021-10-21 17:09:01 -0300

    alter

--- crud-cidades/src/main/java/br/edu/utfpr/cp/javaweb/crudcidades/visao/Cidades.java
+++ crud-cidades/src/main/java/br/edu/utfpr/cp/javaweb/crudcidades/visao/Cidades.java
@@ -1,10 +1,17 @@
 package br.edu.utfpr.cp.javaweb.crudcidades.visao;
 
-
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 public class Cidades {
 
+	
+	@NotBlank(message = "Nome da cidade deve ser informado")
+	@Size(min = 5, max = 60, message = "O nome da cidade deve ter entre 5 e 60 caracteres")
 	private final String nome;
+	
+	@NotBlank(message = "Sigla do estado deve ser informado")
+	@Size(min = 5, max = 60, message = "A Sigla do estado esta limitada a dois caracteres")
 	private final String estado;
 	
 	
@@ -15,7 +22,6 @@ public class Cidades {
 
 
 	
-	//@NotBlank(message = "Nome da cidade deve ser informado")
 	public String getNome() {
 		return nome;
 	}
